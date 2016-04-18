008fb69094e1eb29584a5b4ccb07e4f0e82fdb39
wangefan <wangefan@gmail.com>
2016-04-18 08:08:45 +0000

    [What]To detect flip in SessionsView

--- app/src/main/java/com/te/UI/SessionsView.java
+++ app/src/main/java/com/te/UI/SessionsView.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import com.example.terminalemulation.R;
 import android.content.Context;
+import android.support.graphics.drawable.BuildConfig;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class SessionsView extends ListView {
+public class SessionsView extends ListView implements View.OnTouchListener {
+	//Inner classes begin
 	private class SessionItem {
 		String mTitle;
 		SessionItem (String title) {
@@ -62,24 +67,80 @@ public class SessionsView extends ListView {
 			public TextView mTvTitle;
 		}
 	}
-	
+
+	private class MySimpleGesture implements GestureDetector.OnGestureListener {
+		@Override
+		public boolean onDown(MotionEvent e) {
+			if(BuildConfig.DEBUG) {
+				Log.d("TE", "onDown");
+			}
+			return false;
+		}
+
+		@Override
+		public void onShowPress(MotionEvent e) {
+
+		}
+
+		@Override
+		public boolean onSingleTapUp(MotionEvent e) {
+			return false;
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+			return false;
+		}
+
+		@Override
+		public void onLongPress(MotionEvent e) {
+
+		}
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			if(BuildConfig.DEBUG) {
+				Log.d("TE", "onFling");
+			}
+			final int FLING_MIN_DISTANCE = 100, FLING_MIN_VELOCITY = 200;
+			if (e2.getX() - e1.getX() > FLING_MIN_DISTANCE
+					&& Math.abs(velocityX) > FLING_MIN_VELOCITY) {
+				// Fling right
+				if(BuildConfig.DEBUG) {
+					Log.d("TE-MyGesture", "Fling right");
+				}
+				int pos = pointToPosition((int)e1.getX(), (int)e1.getY());
+				View item = getChildAt(pos);
+				if (item != null) {
+					if(BuildConfig.DEBUG)  Log.d("TE-MyGesture", "item = " + String.valueOf(pos));
+				}
+			}
+			return false;
+		}
+	}
+	//Inner classes end
+
 	//Members
 	private LayoutInflater mInflater;
 	private List<SessionItem> mSessionItems = new ArrayList<SessionItem>();
-	
+	private GestureDetector mGestDetector = new GestureDetector(getContext(), new MySimpleGesture());
+
 	public SessionsView(Context context) {
 		super(context);
 		this.setAdapter(new SessionItemsAdapter(context));
+		setOnTouchListener(this);
 	}
 	
 	public SessionsView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.setAdapter(new SessionItemsAdapter(context));
+		setOnTouchListener(this);
 	}
 		
 	public SessionsView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		this.setAdapter(new SessionItemsAdapter(context));
+		setOnTouchListener(this);
 	}
 	
 	//Functions
@@ -93,5 +154,10 @@ public class SessionsView extends ListView {
 	
 	public String getSessionTitle(int nPos) {
 		return mSessionItems.get(nPos).mTitle;
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		return mGestDetector.onTouchEvent(event);
 	}
 }
